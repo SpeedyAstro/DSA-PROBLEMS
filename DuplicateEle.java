d8f782b7a31ba6529ce05f5e3d9c10a59e0f96fb
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-05 13:22:32 +0530

    Array

--- DuplicateEle.java
+++ DuplicateEle.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DuplicateEle {
     public static int FindDuplicate(int [] arr){
         int ans =0;
@@ -9,6 +11,15 @@ public class DuplicateEle {
         }
         return ans;
     }
+        public int findDuplicate(int[] nums) { 
+            Arrays.sort(nums);
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i-1])
+                    return nums[i];
+            }
+    
+            return -1;
+        }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,5};
         int n = FindDuplicate(arr);
