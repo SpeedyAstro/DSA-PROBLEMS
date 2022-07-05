ffc4960913dca63a59bdcb44c12b3ac62985677b
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-05 11:49:11 +0530

    Array

--- UniqueEle.java
+++ UniqueEle.java
@@ -1,5 +1,20 @@
 class UniqueEle{
+    /*
+     * You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
+Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
+You need to find and return that number which is unique in the array/list.
+     */
+    public static int FindUniqueEle(int [] arr){
+        int ans=0;
+        for (int ele : arr) {
+            ans = ele^ans;
+        }
+        return ans;
+    } 
+    
     public static void main(String[] args) {
-        
+        int arr[] = {2,3,1,6,3,6,2};
+        int n = FindUniqueEle(arr);
+        System.out.println(n);
     }
 }
