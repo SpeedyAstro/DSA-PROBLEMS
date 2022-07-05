d614fdfc94510d71a712bc699cc3edc19c098012
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-05 19:40:51 +0530

    Array

--- ArrOfDuplicates.java
+++ ArrOfDuplicates.java
@@ -1,9 +1,14 @@
+/*
+ * Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+ */
 import java.util.ArrayList;
 
 public class ArrOfDuplicates {
     public static ArrayList<Integer> DuplicateFind(int[] arr){
-        int occ[] = new int[arr.length+1]; // 
-        ArrayList<Integer> ans = new ArrayList<>();
+        int occ[] = new int[arr.length+1]; // array keep the occurance of element 
+        ArrayList<Integer> ans = new ArrayList<>(); //
         for(int i = 0;i < arr.length;i++){
             occ[arr[i]]++;
         }
