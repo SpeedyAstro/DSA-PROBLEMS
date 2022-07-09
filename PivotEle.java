ecb42b6ac74e9ec8f28599c7815c6e3b11369c39
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-10 00:14:28 +0530

    Array

---
+++ PivotEle.java
@@ -0,0 +1,21 @@
+// Approach - binary Search
+public class PivotEle {
+    private static int FindPivot(int [] arr ){
+        int start = 0, end = arr.length;
+        int mid = start + (end-start)/2;
+        while(start<end){
+            if (arr[mid] >= arr[0]) {
+                start = mid +1 ;
+            }
+            else{
+                end = mid;
+            }
+            mid = start + (end-start)/2;
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        int [] arr = {3,8,10,17,1};
+        System.out.println("Pivot Element is at : "+FindPivot(arr)+"\nValue : "+ arr[FindPivot(arr)]);
+    }
+}
