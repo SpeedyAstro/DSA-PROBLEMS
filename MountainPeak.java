1ea3b34eb4a441bc76a70bb68989182707b133d6
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-09 23:26:22 +0530

    Array

--- MountainPeak.java
+++ MountainPeak.java
@@ -1,3 +1,14 @@
+/*
+ * An array arr a mountain if the following properties hold:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+        Input: arr = [0,1,0]
+        Output: 1
+    [APPROACH] - Binary Search
+ */
 public class MountainPeak {
     private static int PeakElement(int[] arr){
         int start = 0, end = arr.length-1;
