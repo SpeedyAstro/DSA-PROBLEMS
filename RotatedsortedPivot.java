b875d6cd2d8351c411ad1177f771ae9d6cebd269
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-10 23:54:51 +0530

    Array

--- RotatedsortedPivot.java
+++ RotatedsortedPivot.java
@@ -2,9 +2,9 @@ public class RotatedsortedPivot {
     private static int SearchBinary(int[] arr,int key,int start, int end){
         int s = start, e = end;
         int mid = s + (e-s)/2;
-        while(s<e){
+        while(s<=e){
             if(arr[mid] == key){
-                return s;
+                return mid;
             }
             else if(arr[mid]>key){
                 e = mid-1;
@@ -14,12 +14,12 @@ public class RotatedsortedPivot {
             }
             mid = s + (e-s)/2;
         }
-        return s;
+        return -1;
     }
     private static int SearchPivot(int[] arr){
         int start = 0, end = arr.length-1;
         int mid = start + (end-start)/2;
-        while(start<=end){
+        while(start<end){
             if(arr[mid]>arr[0]){
                 start = mid+1;
             }
@@ -34,13 +34,16 @@ public class RotatedsortedPivot {
         int pivot = SearchPivot(arr);
         int n = arr.length-1;
         if(key >= arr[pivot] && key <= arr[arr.length-1]){
+            // BS ON SECOND LINE
             return SearchBinary(arr, key,pivot,n);
         }
         else{
+            // BS ON FIRST LINE
             return SearchBinary(arr, key, 0, pivot-1);
         }
     }
     public static void main(String[] args) {
-        
+        int [] arr = {7,8,1,3,5};
+        System.out.println(FindKey(arr, 5));
     }
 }
