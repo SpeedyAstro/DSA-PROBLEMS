2cbb17c0ddf5766dbc7c1cc3ce8f751af9a55f1a
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-18 22:37:32 +0530

    Array

--- FindSqrRoot.java
+++ FindSqrRoot.java
@@ -19,8 +19,21 @@ class FindSqrRoot{
         }
         return ans;
     }
+    private static double GetPrecision(int n,int temp,int precision){
+        double factor = 1;
+        double ans = temp;
+        for(int i = 0;i<precision;i++){
+            factor = factor / 10;
+            for(double j = ans;j*j<n;j= j+factor){
+                ans = j;
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,6,7,8,9};
-        System.out.println(SearchSqrRoot(arr, 25));
+        System.out.println(SearchSqrRoot(arr, 26));
+        int tempSol = SearchSqrRoot(arr, 101);
+        System.out.println(GetPrecision(101, tempSol, 6));
     }
 }
