9863d56f09eda39bfff17b67046147d69a0a53a7
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-08-01 13:58:00 +0530

    String

---
+++ AntonandPolyhedrons.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AntonandPolyhedrons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            String s = sc.next();
+            if(s.equals("Tetrahedron")){
+                counter = counter + 4;
+            }
+            else if(s.equals("Cube")){
+                counter += 6;
+            }
+            else if(s.equals("Octahedron")){
+                counter += 8;
+            }
+            else if(s.equals("Dodecahedron")){
+                counter += 12;
+            }
+            else if(s.equals("Icosahedron")){
+                counter += 20;
+            }
+        }
+        System.out.println(counter);
+        sc.close();
+    }
+}
