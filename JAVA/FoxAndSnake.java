6ef62a2175429b0dd8b1e3f1bc5ec6d0db0fdb15
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-08-02 21:43:00 +0530

    Snake-Matrix

--- JAVA/FoxAndSnake.java
+++ JAVA/FoxAndSnake.java
@@ -11,7 +11,7 @@ class FoxAndSnake{
         int k;
         int n = sc.nextInt();
         int m = sc.nextInt();
-        char [][] table = new char[n][m];
+        char [][] table = new char[n][m]; 
         for(int i = 0; i < n ; i++){
             for(int j = 0 ; j < m; j++){
                 if((i+1)%2 != 0){
