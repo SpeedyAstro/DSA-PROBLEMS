6e19eb4a32c9293effc778e8df28adf5bed62c06
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-08-02 21:36:31 +0530

    BeautifulMatrix

--- JAVA/BeautifulMatrix.java
+++ JAVA/BeautifulMatrix.java
@@ -6,7 +6,7 @@
 
 Swap two neighboring matrix rows, that is, rows with indexes i and i + 1 for some integer i (1 ≤ i < 5).
 Swap two neighboring matrix columns, that is, columns with indexes j and j + 1 for some integer j (1 ≤ j < 5).
-You think that a matrix looks beautiful, if the single number one of the matrix is located in its middle (in the cell that is on the intersection of the third row and the third column). Count the minimum number of moves needed to make the matrix beautiful.
+You think that a matrix looks beautiful, if the single number one of the matrix is located in its middle (in the cell that is on the intersection of the third row and the third column). Count the minimum number of moves needed to make the matrix beautiful. 
 
 Input
 The input consists of five lines, each line contains five integers: the j-th integer in the i-th line of the input represents the element of the matrix that is located on the intersection of the i-th row and the j-th column. It is guaranteed that the matrix consists of 24 zeroes and a single number one.
@@ -31,6 +31,6 @@ public class BeautifulMatrix {
             }
         }
         sc.close();
-    System.out.println(value);
+    System.out.println(value); 
     }
 }
