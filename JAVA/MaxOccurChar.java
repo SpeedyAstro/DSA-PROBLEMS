65ebda6cb67885dc896660b552e9c7927ceb7b1a
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-08-02 21:44:52 +0530

    Maxoccurance

--- JAVA/MaxOccurChar.java
+++ JAVA/MaxOccurChar.java
@@ -16,7 +16,7 @@ public class MaxOccurChar {
         System.out.println("Maximum Occuring Character is : "+c);
         System.out.println("Number of times occured : "+max);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         FindMaxChar(s);
