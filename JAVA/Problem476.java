f5795e2dcdf4fea8ff3b87d2c4d4254103538987
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-08-23 00:01:16 +0530

    Dreamoon and Stairs

--- JAVA/Problem476.java
+++ JAVA/Problem476.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
-
+/*
+ * Dreamoon wants to climb up a stair of n steps. He can climb 1 or 2 steps at each move. Dreamoon wants the number of moves to be a multiple of an integer m.
+ * What is the minimal number of moves making him climb to the top of the stairs that satisfies his condition?
+ * Maximum number of moves possible - 1,1,1,1,1,1,1,1,...
+ * Minimum number of moves possible - n/2;
+ * approach:
+ * incrementing n/2 to until it is divisible by m 
+ * 
+ */
 public class Problem476 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,5 +29,6 @@ public class Problem476 {
             }
         }
         System.out.println(x);
+        sc.close();
     }
 }
